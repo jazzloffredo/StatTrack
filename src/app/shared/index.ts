cecc100bc25c72e257df04821e19684cceac9e97
Jazz Loffredo <jloffredo@users.campus>
2019-04-05 15:35:15 -0500

    Added barrel file for model exports

---
+++ src/app/shared/index.ts
@@ -0,0 +1,19 @@
+// User Classes
+export * from './models/user/user';
+
+// Season Classes
+export * from './models/season/season';
+
+// Team Classes
+export * from './models/team/team';
+export * from './models/team/team-season';
+export * from './models/team/team-game';
+export * from './models/team/team-game-stats';
+
+// Player Classes
+export * from './models/player/player';
+export * from './models/player/player-season';
+export * from './models/player/player-game';
+export * from './models/player/player-offensive-game-stats';
+export * from './models/player/player-defensive-game-stats';
+
