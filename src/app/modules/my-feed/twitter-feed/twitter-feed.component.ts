3f75030000384e8d0e0d6a5df4028ddd66512cb0
Jazz Loffredo <jloffredo@users.campus>
2019-04-29 15:48:33 -0500

    Added some more stuff

--- src/app/modules/my-feed/twitter-feed/twitter-feed.component.ts
+++ src/app/modules/my-feed/twitter-feed/twitter-feed.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Tweet } from 'src/app/shared/models/twitter/tweet';
+
 @Component({
   selector: 'app-twitter-feed',
   templateUrl: './twitter-feed.component.html',
   styleUrls: ['./twitter-feed.component.scss']
 })
 export class TwitterFeedComponent implements OnInit {
+
+  returnedTweets: Tweet[];
 
   constructor() { }
 
